bd6360f80aaa66b4f2d2b5a2826d377ddb6eeeb0
Gautham P <gauthampdn@gmail.com>
2024-10-18 18:03:33 -0700

    i think im not a fruit

--- pergifrontend/src/pages/PublicAssignment.js
+++ pergifrontend/src/pages/PublicAssignment.js
@@ -145,11 +145,10 @@ const PublicAssignment = () => {
       }
     }
   };
-  
 
 
   async function getTextFromPdf(file) {
-    const pdfjsLib = await import('pdfjs-dist/build/pdf.js');
+    const pdfjsLib = await import('pdfjs-dist/build/pdf.mjs');
     // const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry.mjs');
 
     // pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
