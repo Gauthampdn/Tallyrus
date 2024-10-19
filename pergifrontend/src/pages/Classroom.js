9b6f4c0b753d75c391c7f5c70bf5e37f7394b97b
Gautham P <gauthampdn@gmail.com>
2024-10-18 17:53:22 -0700

    i hate monkeys

--- pergifrontend/src/pages/Classroom.js
+++ pergifrontend/src/pages/Classroom.js
@@ -488,7 +488,7 @@ const Classroom = () => {
 
 
   async function getTextFromPdf(file) {
-    const pdfjsLib = await import('pdfjs-dist/build/pdf.mjs');
+    const pdfjsLib = await import('pdfjs-dist/build/pdf.js');
     // const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry.mjs');
 
     // pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
