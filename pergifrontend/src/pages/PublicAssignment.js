159fe1bb6067c1e8e26f9d2f65aa61281b12a68f
Gautham P <gauthampdn@gmail.com>
2025-02-12 14:36:21 -0800

    went back to mjs

--- pergifrontend/src/pages/PublicAssignment.js
+++ pergifrontend/src/pages/PublicAssignment.js
@@ -148,7 +148,7 @@ const PublicAssignment = () => {
 
 
   async function getTextFromPdf(file) {
-    const pdfjsLib = await import('pdfjs-dist/build/pdf.js');
+    const pdfjsLib = await import('pdfjs-dist/build/pdf.mjs');
     // const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry.mjs');
 
     // pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
