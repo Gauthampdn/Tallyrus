8db0a955fc825fa7a5ceb2254afd86aef37d56b7
Denil Neil <denilcollins@gmail.com>
2025-03-12 14:19:45 -0700

    revert authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:4000/auth/redirect/google", // somehow this is appending /undefined after ENV so i hardcoded it
+  callbackURL: "https://bcknd.tallyrus.com/auth/redirect/google", // somehow this is appending /undefined after ENV so i hardcoded it
   passReqToCallback: true
 },
 
@@ -32,7 +32,7 @@ passport.use(new GoogleStrategy({
           id: profile.id,
           picture: profile.picture,
           name: profile.given_name,
-          authority: "student",
+          authority: "teacher",
           numGraded: 0
         });
         await user.save();
@@ -66,15 +66,15 @@ passport.deserializeUser(async function (id, done) {
 const getAuth = passport.authenticate("google", { scope: ["email", "profile"] });
 
 const redirectGoogle = passport.authenticate("google", {
-  successRedirect: "http://localhost:3000/app",
-  failureRedirect: "http://localhost:3000/login",
+  successRedirect: "https://tallyrus.com/app",
+  failureRedirect: "https://tallyrus.com/login",
 });
 
 const logout = (req, res) => {
   req.logout(() => {
     req.session.destroy(() => {
       res.clearCookie('connect.sid');
-      res.redirect('http://localhost:3000/login');
+      res.redirect('https://tallyrus.com');
     });
   });
 }
